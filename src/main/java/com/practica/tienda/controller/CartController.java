9b676301ef33f24cf0f6d3ff097cda270aab26a2
Eduardo Acosta <eacosta@ipcom.com.mx>
2025-12-25 22:35:04 -0600

    Se agregaron logs y generación del contrato y lectura de contrato

--- src/main/java/com/practica/tienda/controller/CartController.java
+++ src/main/java/com/practica/tienda/controller/CartController.java
@@ -2,16 +2,18 @@ package com.practica.tienda.controller;
 
 import com.practica.tienda.model.Product;
 import com.practica.tienda.service.CartService;
+import lombok.extern.slf4j.Slf4j; //Hay que importar la libreria para utilizar @Slf4j
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RestController //
 @RequestMapping("/api/cart") //
-@CrossOrigin(origins = "http://localhost:5173") //
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true") //
 
-//Aqui el front(react) realiza las peticiones (Obtener la sesión, agregar el producto a la sesión y eliminar el producto a la sesión)
+//Aquí el front(react) realiza las peticiones (Obtener la sesión, agregar el producto a la sesión y eliminar el producto a la sesión)
 public class CartController {
 
     private final CartService cartService;
@@ -22,8 +24,9 @@ public class CartController {
 
     @PostMapping("/{sessionId}/items")
     public ResponseEntity<?> addItem(@PathVariable String sessionId, @RequestBody Product product) {
-        // Valida si el producto que queremos añadir tiene cantidad mayor a 1, si no devuelve error y su mensaje
+        // Válida si el producto que queremos añadir tiene cantidad mayor a 1, si no devuelve error y su mensaje
         if (product.getQuantity() == null || product.getQuantity() < 1) {
+            log.warn("El producto {} ya no tiene stock en sesion: {}",product, sessionId); // Log de error
             return ResponseEntity.badRequest().body(Map.of(
                     "status", 400,
                     "message", "La cantidad debe ser mayor o igual a 1",
@@ -33,22 +36,24 @@ public class CartController {
         }
         //En caso de que su cantidad sea mayor a 1, se añade el producto
         cartService.addProduct(sessionId, product);
+        log.info("Producto {} agregado al carrito exitosamente para el usuario:.",product, sessionId);
         return ResponseEntity.ok().build();
     }
 
-    //obtiene la lista de los prductos guardados
+    //obtiene la lista de los productos guardados
     //@Pathvariable obtiene el id de la sesión
     @GetMapping("/{sessionId}")
     public List<Product> getCart(@PathVariable String sessionId) {
+        log.info("Consulta de carrito recibida para la sesión: {}", sessionId); // Log de información
         return cartService.getCart(sessionId);
     }
 
-    /*Obtiene el id de sesión y el id de producto seleccionado y llama al metodo del servico para que lo elimine*/
+    /*Obtiene el id de sesión y el id de producto seleccionado y llama al metodo del servicio para que lo elimine*/
     @DeleteMapping("/{sessionId}/items/{productId}")
     public ResponseEntity<?> removeItem(
             @PathVariable String sessionId,
             @PathVariable Long productId) {
-
+        log.warn("Petición de borrado para el producto ID: {} en sesión: {}", productId, sessionId); // Log de advertencia
         cartService.removeProduct(sessionId, productId);
         return ResponseEntity.ok().build(); //
     }
--- src/main/java/com/practica/tienda/service/CartService.java
+++ src/main/java/com/practica/tienda/service/CartService.java
@@ -4,10 +4,10 @@ import com.practica.tienda.model.Product;
 import org.springframework.stereotype.Service;
 import java.util.*;
 
-/*Es la clase que sabe como agregar un prodcuto al carrito, obtener el detalle del carrito y poder eliminaro
+/*Es la clase que sabe como agregar un producto al carrito, obtener el detalle del carrito y poder eliminarlo
 y se guarda en memoria temporal todos esos movimientos */
 
-@Service //Indica que la clase tiene la logica de negocio y actua como intermediario
+@Service //Indica que la clase tiene la lógica de negocio y actúa como intermediario
 
 public class CartService {
     // Permite que los usuarios tengan diferentes carritos al mismo tiempo y no se mezclen
@@ -15,11 +15,13 @@ public class CartService {
 
     //si el usuario no tiene un carrito, se crea uno con su id de sesión y añade el producto seleccionado
     public void addProduct(String sessionId, Product product) {
+        //Se va añadiendo un producto a la lista, entonces en el app.tsx, solo obtiene la longitud de esa lista, en este caso, el total de los productos que se añadieron
         carts.computeIfAbsent(sessionId, k -> new ArrayList<>()).add(product);
+
     }
 
-    /*Si el usuario ha agreado prodcutos al carrito, se obtiene el carrito de acuerdo con el id de sesión,
-    en caso de no tener nada, se manda la lista vacia*/
+    /*Si el usuario agregó productos al carrito, se obtiene el carrito de acuerdo con el id de sesión,
+    en caso de no tener nada, se manda la lista vacía*/
     public List<Product> getCart(String sessionId) {
         return carts.getOrDefault(sessionId, new ArrayList<>());
     }
@@ -27,8 +29,8 @@ public class CartService {
     public void removeProduct(String sessionId, Long productId) {
         List<Product> cart = carts.get(sessionId);
 
-        /*si el carrito no esta vacio, busca el solo el primer producto que coincida con el id
-          si lo encuentra, realiza el borrado del prodcuto*/
+        /*si el carrito no está vacío, busca solo el primer producto que coincida con el id
+          si lo encuentra, realiza el borrado del producto*/
         if (cart != null) {
             Optional<Product> itemToRemove = cart.stream()
                     .filter(p -> p.getId().equals(productId))
